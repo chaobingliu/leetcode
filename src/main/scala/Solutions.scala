d86539efc0ec73b9370b598773de946a03b6f6d6
liu.chaobing <liu.chaobing@chinaredstar.com>
2020-10-19 21:22:29 +0800

    update

--- src/main/scala/Solutions.scala
+++ src/main/scala/Solutions.scala
@@ -91,7 +91,8 @@ object Solutions {
     //    minWindow("ADOBECODEBANC", "ABC")
     //    subsets(Array(1, 2, 3))
     //    println(minWindow("ADOBECODEBANC", "ABC"))
-    println(exist(Array(Array('A', 'B', 'C', 'E'), Array('S', 'F', 'C', 'S'), Array('A', 'D', 'E', 'E')), "SEE"))
+    //    println(exist(Array(Array('A', 'B', 'C', 'E'), Array('S', 'F', 'C', 'S'), Array('A', 'D', 'E', 'E')), "SEE"))
+    println(largestRectangleArea(Array(3, 6, 5, 7, 4, 8, 1, 0)))
 
   }
 
@@ -1740,6 +1741,58 @@ board 和 word 中只包含大写和小写英文字母。
     }
     used(rowIdx)(colIdx) = false
     return false
+  }
+
+  /*
+  84. 柱状图中最大的矩形
+给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+
+求在该柱状图中，能够勾勒出来的矩形的最大面积。
+
+
+
+
+
+以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
+
+
+
+
+
+图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。
+
+
+
+示例:
+
+输入: [2,1,5,6,2,3]
+输出: 10
+   */
+  def largestRectangleArea(heights: Array[Int]): Int = {
+    if (heights.length == 0)
+      return 0
+    if (heights.length == 1)
+      return heights(0)
+
+    import scala.collection.mutable
+    val stack: mutable.Stack[Int] = new mutable.Stack[Int]()
+    var max = 0
+
+    for (i <- 0 until heights.length) {
+      while (!stack.isEmpty && heights(stack.top) > heights(i)) {
+        val popIdx = stack.pop
+        val width = if (stack.isEmpty) i else (i - stack.top - 1)
+        max = Math.max(max, heights(popIdx) * width)
+      }
+      stack.push(i)
+    }
+
+    while (!stack.isEmpty) {
+      val popIdx = stack.pop()
+      val width = if (stack.isEmpty) heights.length else (heights.length - stack.top - 1)
+      max = Math.max(max, heights(popIdx) * width)
+    }
+    max
   }
 
 }
