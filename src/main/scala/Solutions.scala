7a7a03bf1a201895f79ff4f20c02f2b841efbe75
danny <chaobingliui@163.com>
2020-10-20 23:53:05 +0800

    update

--- src/main/scala/Solutions.scala
+++ src/main/scala/Solutions.scala
@@ -1901,6 +1901,22 @@ board 和 word 中只包含大写和小写英文字母。
 
   }
 
+  /*
+  94. 二叉树的中序遍历
+给定一个二叉树，返回它的中序 遍历。
+
+示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,3,2]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+   */
   def inorderTraversal(root: TreeNode): List[Int] = {
     import scala.collection.mutable
     import scala.collection.mutable.ListBuffer
@@ -2068,7 +2084,7 @@ board 和 word 中只包含大写和小写英文字母。
 
   def rec(leftTree: TreeNode, rightTree: TreeNode): Boolean = {
     if (leftTree == null && rightTree == null) {
-      return true
+      true
     } else if (leftTree != null && rightTree != null) {
       if (leftTree.value == rightTree.value) {
         rec(leftTree.left, rightTree.right) && rec(leftTree.right, rightTree.left)
