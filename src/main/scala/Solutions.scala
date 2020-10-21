9a91d2a8ecef912f67eafe1605def021deb272ff
liu.chaobing <liu.chaobing@chinaredstar.com>
2020-10-21 17:59:38 +0800

    update

--- src/main/scala/Solutions.scala
+++ src/main/scala/Solutions.scala
@@ -124,7 +124,8 @@ object Solutions {
     //    flatten(A)
     //    println(maxProfit(Array(7, 1, 5, 3, 6, 4)))
     //    println(maxPathSum(A))
-    println(longestConsecutive(Array(0, 0)))
+    //    println(longestConsecutive(Array(0, 0)))
+    println(singleNumber(Array(4, 1, 2, 1, 2)))
   }
 
   /*
@@ -2426,6 +2427,27 @@ board 和 word 中只包含大写和小写英文字母。
       }
     }
     max
+  }
+
+  /*
+  136. 只出现一次的数字
+给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+说明：
+
+你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+示例 1:
+
+输入: [2,2,1]
+输出: 1
+示例 2:
+
+输入: [4,1,2,1,2]
+输出: 4
+   */
+  def singleNumber(nums: Array[Int]): Int = {
+    nums.reduce(_ ^ _)
   }
 
 
