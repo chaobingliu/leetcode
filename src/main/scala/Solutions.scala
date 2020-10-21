3794d961f036a4aa757372121af8e54f0e979d08
liu.chaobing <liu.chaobing@chinaredstar.com>
2020-10-21 16:44:31 +0800

    update

--- src/main/scala/Solutions.scala
+++ src/main/scala/Solutions.scala
@@ -123,8 +123,8 @@ object Solutions {
     //    buildTree(Array(4,1,2,3), Array(1,2,3,4))
     //    flatten(A)
     //    println(maxProfit(Array(7, 1, 5, 3, 6, 4)))
-    println(maxPathSum(A))
-
+    //    println(maxPathSum(A))
+    println(longestConsecutive(Array(1, 0, -1)))
   }
 
   /*
@@ -2397,6 +2397,37 @@ board 和 word 中只包含大写和小写英文字母。
     maxNode.value = Math.max(leftMax + rightMax + root.value, maxNode.value)
 
     return root.value + Math.max(leftMax, rightMax)
+  }
+
+  /*
+  128. 最长连续序列
+给定一个未排序的整数数组，找出最长连续序列的长度。
+
+要求算法的时间复杂度为 O(n)。
+
+示例:
+
+输入: [100, 4, 200, 1, 3, 2]
+输出: 4
+解释: 最长连续序列是 [1, 2, 3, 4]。它的长度为 4。
+   */
+  def longestConsecutive(nums: Array[Int]): Int = {
+    if (nums.isEmpty)
+      return 0
+    val set: Set[Int] = nums.toSet
+    var max = 1
+    for (num <- set) {
+      if (set.contains(num - 1)) {
+        var currentNumber = num - 2
+        var currentLength = 2
+        while (set.contains(currentNumber)) {
+          currentLength += 1
+          currentNumber -= 1
+        }
+        max = Math.max(max, currentLength)
+      }
+    }
+    max
   }
 
 }
