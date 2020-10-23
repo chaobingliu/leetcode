ade71c4cf1755b254b97d63ab333efdd87ed2f66
liu.chaobing <liu.chaobing@chinaredstar.com>
2020-10-23 21:34:46 +0800

    update

--- src/main/scala/Solutions.scala
+++ src/main/scala/Solutions.scala
@@ -1,4 +1,4 @@
-
+import scala.collection.mutable.ArrayBuffer
 
 object Solutions {
   def main(args: Array[String]): Unit = {
@@ -142,7 +142,8 @@ object Solutions {
     //    val d = new ListNode(2)
     //    d.next = a
     //    getIntersectionNode(a, a)
-    println(rob(Array(1, 2, 3, 1)))
+//    println(rob(Array(1, 2, 3, 1)))
+    println(numIslands(Array(Array('1','1','1','1','0'),Array('1','1','0','1','0'),Array('1','1','0','0','0'),Array('0','0','0','0','0'))))
   }
 
   /*
@@ -2857,5 +2858,79 @@ pos 的值为 -1 或者链表中的一个有效索引
       dp(i) = Math.max(nums(i) + dp(i - 2), dp(i - 1))
     }
     dp(len - 1)
+  }
+
+  /*
+  200. 岛屿数量
+给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+
+岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+
+此外，你可以假设该网格的四条边均被水包围。
+
+
+
+示例 1：
+
+输入：grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+输出：1
+示例 2：
+
+输入：grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+输出：3
+
+
+提示：
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] 的值为 '0' 或 '1'
+   */
+  def numIslands(grid: Array[Array[Char]]): Int = {
+    if (grid == null || grid.isEmpty)
+      return 0
+
+    val row = grid.length
+    val col = grid(0).length
+    var g :ArrayBuffer[ArrayBuffer[Char]] = ArrayBuffer[ArrayBuffer[Char]]()
+    for (i <- 0 until row) {
+      var af: ArrayBuffer[Char] = new ArrayBuffer[Char]()
+      for (j <- 0 until col) {
+        af += (grid(i)(j))
+      }
+      g += af
+    }
+
+
+    var count: Int = 0
+    for (i <- 0 until row; j <- 0 until col) {
+      if (grid(i)(j) == '1') {
+        count += 1
+        dfsNumIsLands(g, i, j, row, col)
+      }
+    }
+    count
+  }
+
+  def dfsNumIsLands(g: ArrayBuffer[ArrayBuffer[Char]], r: Int, c: Int, row: Int, col: Int): Unit = {
+    if (c < 0 || r < 0 || c >= col || r >= row || g(r)(c) == '0')
+      return
+
+    g(r)(c) == '0'
+    dfsNumIsLands(g, r - 1, c, row, col)
+    dfsNumIsLands(g, r + 1, c, row, col)
+    dfsNumIsLands(g, r, c - 1, row, col)
+    dfsNumIsLands(g, r, c + 1, row, col)
   }
 }
