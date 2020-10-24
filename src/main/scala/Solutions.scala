30dcf54490ac39cf2f6e53f89c4056d747e887c4
danny <chaobingliui@163.com>
2020-10-24 13:59:00 +0800

    update

--- src/main/scala/Solutions.scala
+++ src/main/scala/Solutions.scala
@@ -144,12 +144,13 @@ object Solutions {
     //    getIntersectionNode(a, a)
     //    println(rob(Array(1, 2, 3, 1)))
     //    println(numIslands(Array(Array('1', '1', '1', '1', '0'), Array('1', '1', '0', '1', '0'), Array('1', '1', '0', '0', '0'), Array('0', '0', '0', '0', '0'))))
-    val a = new ListNode(3)
-    val b = new ListNode(2)
-    val d = new ListNode(1)
-    a.next = b
-    b.next = d
-    println(reverseList(a))
+    //    val a = new ListNode(3)
+    //    val b = new ListNode(2)
+    //    val d = new ListNode(1)
+    //    a.next = b
+    //    b.next = d
+    //    println(reverseList(a))
+    println(canFinish(3, Array(Array(1, 0), Array(2, 1))))
   }
 
   /*
@@ -2959,4 +2960,68 @@ grid[i][j] 的值为 '0' 或 '1'
     curNode.next = null
     retNode
   }
+
+  /*
+  207. 课程表
+你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。
+
+在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们：[0,1]
+
+给定课程总量以及它们的先决条件，请你判断是否可能完成所有课程的学习？
+
+
+
+示例 1:
+
+输入: 2, [[1,0]]
+输出: true
+解释: 总共有 2 门课程。学习课程 1 之前，你需要完成课程 0。所以这是可能的。
+示例 2:
+
+输入: 2, [[1,0],[0,1]]
+输出: false
+解释: 总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0；并且学习课程 0 之前，你还应先完成课程 1。这是不可能的。
+
+
+提示：
+
+输入的先决条件是由 边缘列表 表示的图形，而不是 邻接矩阵 。详情请参见图的表示法。
+你可以假定输入的先决条件中没有重复的边。
+1 <= numCourses <= 10^5
+   */
+  def canFinish(numCourses: Int, prerequisites: Array[Array[Int]]): Boolean = {
+    import scala.collection.mutable.ListBuffer
+    import scala.collection.mutable
+    val edges: Array[ListBuffer[Int]] = new Array[ListBuffer[Int]](numCourses)
+    val inedge: Array[Int] = new Array[Int](numCourses)
+    for (i <- 0 until numCourses) {
+      edges(i) = new ListBuffer[Int]()
+    }
+
+    for (arr <- prerequisites) {
+      edges(arr(1)).append(arr(0))
+      inedge(arr(0)) += 1
+    }
+
+    val queue: mutable.Queue[Int] = new mutable.Queue[Int]()
+    for (i <- 0 until numCourses) {
+      if (inedge(i) == 0) {
+        queue.enqueue(i)
+      }
+    }
+
+    var retCount = 0
+    while (!queue.isEmpty) {
+      val pre = queue.dequeue()
+      retCount += 1
+      for (ed <- edges(pre)) {
+        inedge(ed) -= 1
+        if (inedge(ed) == 0) {
+          queue.enqueue(ed)
+        }
+      }
+    }
+    retCount == numCourses
+  }
+
 }
