6db4f2ff39f0a901806bffcb5e9ed0c32b63fc87
danny <chaobingliui@163.com>
2020-10-25 09:41:34 +0800

    update

--- src/main/scala/Solutions.scala
+++ src/main/scala/Solutions.scala
@@ -149,7 +149,9 @@ object Solutions {
     //    b.next = d
     //    println(reverseList(a))
     //    println(canFinish(3, Array(Array(1, 0), Array(2, 1))))
-    println(findKthLargest(Array(3, 2, 1, 5, 6, 4), 2))
+    //    println(findKthLargest(Array(3, 2, 1, 5, 6, 4), 2))
+
+    println(maximalSquare(Array(Array('1', '0', '1', '0', '0'), Array('1', '0', '1', '1', '1'), Array('1', '1', '1', '1', '1'), Array('1', '0', '0', '1', '0'))))
   }
 
   /*
@@ -3081,5 +3083,41 @@ grid[i][j] 的值为 '0' 或 '1'
     } else {
       quickSort(arr, q + 1, r, index)
     }
+  }
+
+  /*
+  221. 最大正方形
+在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+
+示例:
+
+输入:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+
+输出: 4
+   */
+  def maximalSquare(matrix: Array[Array[Char]]): Int = {
+    if (matrix == null || matrix.isEmpty || matrix(0).length == 0)
+      return 0
+
+    val row = matrix.length
+    val col = matrix(0).length
+    val dp: Array[Array[Int]] = Array.ofDim(row, col)
+    var maxSide = 0
+    for (i <- 0 until row; j <- 0 until col) {
+      if (matrix(i)(j) == '1') {
+        if (i == 0 || j == 0) {
+          dp(i)(j) = 1
+        } else {
+          dp(i)(j) = Math.min(Math.min(dp(i - 1)(j), dp(i)(j - 1)), dp(i - 1)(j - 1)) + 1
+        }
+        maxSide = Math.max(dp(i)(j), maxSide)
+      }
+    }
+    maxSide * maxSide
   }
 }
