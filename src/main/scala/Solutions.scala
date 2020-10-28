3156500c86a31706b8343ad42bbab7e8feff9331
liu.chaobing <liu.chaobing@chinaredstar.com>
2020-10-28 14:04:02 +0800

    update

--- src/main/scala/Solutions.scala
+++ src/main/scala/Solutions.scala
@@ -3795,4 +3795,56 @@ p、q 为不同节点且均存在于给定的二叉树中。
     }
     if (dp(amount) > amount) -1 else dp(amount)
   }
+
+
+  /*
+    337. 打家劫舍 III
+  在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+
+  计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
+
+  示例 1:
+
+  输入: [3,2,3,null,3,null,1]
+
+       3
+      / \
+     2   3
+      \   \
+       3   1
+
+  输出: 7
+  解释: 小偷一晚能够盗取的最高金额 = 3 + 3 + 1 = 7.
+  示例 2:
+
+  输入: [3,4,5,1,3,null,1]
+
+       3
+      / \
+     4   5
+    / \   \
+   1   3   1
+
+  输出: 9
+  解释: 小偷一晚能够盗取的最高金额 = 4 + 5 = 9.
+     */
+  def rob(root: TreeNode): Int = {
+    val selectedMap = mutable.Map[TreeNode, Int]()
+    val nonselectedMap = mutable.Map[TreeNode, Int]()
+
+    dfs_rob(root: TreeNode, selectedMap, nonselectedMap)
+    Math.max(selectedMap.getOrElse(root, 0), nonselectedMap.getOrElse(root, 0))
+
+
+  }
+
+  def dfs_rob(node: TreeNode, selectedMap: mutable.Map[TreeNode, Int], nonselectedMap: mutable.Map[TreeNode, Int]): Unit = {
+    if (node == null)
+      return
+    dfs_rob(node.left, selectedMap, nonselectedMap)
+    dfs_rob(node.right, selectedMap, nonselectedMap)
+    selectedMap.put(node, node.value + nonselectedMap.getOrElse(node.left, 0) + nonselectedMap.getOrElse(node.right, 0))
+    nonselectedMap.put(node, Math.max(selectedMap.getOrElse(node.left, 0), nonselectedMap.getOrElse(node.left, 0)) +
+      Math.max(selectedMap.getOrElse(node.right, 0), nonselectedMap.getOrElse(node.right, 0)))
+  }
 }
