61583856c5f41364e6824962ad1effa1c7f3ef46
danny <chaobingliui@163.com>
2020-11-08 13:34:39 +0800

    update

--- src/main/scala/study0/Solution.scala
+++ src/main/scala/study0/Solution.scala
@@ -27,7 +27,8 @@ object Solution {
     //    println(removeCoveredIntervals(Array(Array(1, 2), Array(1, 3), Array(3, 2), Array(2, 4))))
     //    println(merge(Array(Array())))
     //    println(threeSum(Array(0, 0, 0)))
-    println(fourSum(Array(1, 0, -1, 0, -2, 2), 0))
+    //    println(fourSum(Array(1, 0, -1, 0, -2, 2), 0))
+    println(superEggDrop(4, 5000))
   }
 
   /*
@@ -1414,5 +1415,91 @@ intervals[i][0] <= intervals[i][1]
       return List[List[Int]]()
     }
     nSum(nums.sorted, 4, 0, target).toList
+  }
+
+  /*
+  887. 鸡蛋掉落
+你将获得 K 个鸡蛋，并可以使用一栋从 1 到 N  共有 N 层楼的建筑。
+
+每个蛋的功能都是一样的，如果一个蛋碎了，你就不能再把它掉下去。
+
+你知道存在楼层 F ，满足 0 <= F <= N 任何从高于 F 的楼层落下的鸡蛋都会碎，从 F 楼层或比它低的楼层落下的鸡蛋都不会破。
+
+每次移动，你可以取一个鸡蛋（如果你有完整的鸡蛋）并把它从任一楼层 X 扔下（满足 1 <= X <= N）。
+
+你的目标是确切地知道 F 的值是多少。
+
+无论 F 的初始值如何，你确定 F 的值的最小移动次数是多少？
+
+
+
+示例 1：
+
+输入：K = 1, N = 2
+输出：2
+解释：
+鸡蛋从 1 楼掉落。如果它碎了，我们肯定知道 F = 0 。
+否则，鸡蛋从 2 楼掉落。如果它碎了，我们肯定知道 F = 1 。
+如果它没碎，那么我们肯定知道 F = 2 。
+因此，在最坏的情况下我们需要移动 2 次以确定 F 是多少。
+示例 2：
+
+输入：K = 2, N = 6
+输出：3
+示例 3：
+
+输入：K = 3, N = 14
+输出：4
+
+
+提示：
+
+1 <= K <= 100
+1 <= N <= 10000
+   */
+  /*
+我们在第i层楼扔了鸡蛋之后，可能出现两种情况：鸡蛋碎了，鸡蛋没碎。注意，这时候状态转移就来了：
+
+如果鸡蛋碎了，那么鸡蛋的个数K应该减一，搜索的楼层区间应该从[1..N]变为[1..i-1]共i-1层楼；
+
+如果鸡蛋没碎，那么鸡蛋的个数K不变，搜索的楼层区间应该从 [1..N]变为[i+1..N]共N-i层楼。
+   */
+  def superEggDrop2(K: Int, N: Int): Int = {
+    val map = mutable.Map[(Int, Int), Int]()
+
+    def dp(K: Int, N: Int): Int = {
+      if (K == 1) return N
+      if (N == 0) return 0
+      if (map.contains((K, N)))
+        return map((K, N))
+      var res = N
+      for (i <- 1 to N) {
+        res = Math.min(res, Math.max(dp(K, N - i), dp(K - 1, i - 1)) + 1)
+      }
+      map((K, N)) = res
+      res
+    }
+
+    dp(K, N)
+  }
+
+  /*
+dp[k][m] = dp[k][m-1] + dp[k-1][m-1] + 1
+
+dp[k][m - 1]就是楼上的楼层数，因为鸡蛋个数k不变，也就是鸡蛋没碎，扔鸡蛋次数m减一；
+
+dp[k - 1][m - 1]就是楼下的楼层数，因为鸡蛋个数k减一，也就是鸡蛋碎了，同时扔鸡蛋次数m减一。
+   */
+  def superEggDrop(K: Int, N: Int): Int = {
+    val dp = Array.ofDim[Int](K + 1, N + 1)
+
+    var m = 0
+    while (dp(K)(m) < N) {
+      m += 1
+      for (i <- 1 to K) {
+        dp(i)(m) = dp(i)(m - 1) + dp(i - 1)(m - 1) + 1
+      }
+    }
+    m
   }
 }
