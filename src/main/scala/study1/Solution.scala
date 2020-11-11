b45b25f18bb3e97f0e415e3425f71bec7cd9414d
liu.chaobing <liu.chaobing@chinaredstar.com>
2020-11-11 11:25:39 +0800

    update

--- src/main/scala/study1/Solution.scala
+++ src/main/scala/study1/Solution.scala
@@ -1,5 +1,7 @@
 package study1
 
+import scala.collection.mutable
+
 class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
   var value: Int = _value
   var left: TreeNode = _left
@@ -116,5 +118,64 @@ object Solution {
       }
     }
     dp(sum)
+  }
+
+  /*
+  72. 编辑距离
+给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+
+你可以对一个单词进行如下三种操作：
+
+插入一个字符
+删除一个字符
+替换一个字符
+
+
+示例 1：
+
+输入：word1 = "horse", word2 = "ros"
+输出：3
+解释：
+horse -> rorse (将 'h' 替换为 'r')
+rorse -> rose (删除 'r')
+rose -> ros (删除 'e')
+示例 2：
+
+输入：word1 = "intention", word2 = "execution"
+输出：5
+解释：
+intention -> inention (删除 't')
+inention -> enention (将 'i' 替换为 'e')
+enention -> exention (将 'n' 替换为 'x')
+exention -> exection (将 'n' 替换为 'c')
+exection -> execution (插入 'u')
+
+
+提示：
+
+0 <= word1.length, word2.length <= 500
+word1 和 word2 由小写英文字母组成
+   */
+  def minDistance(word1: String, word2: String): Int = {
+    val dict = mutable.Map[(Int, Int), Int]()
+
+    def dp(i: Int, j: Int): Int = {
+      if (i == -1) return j + 1
+      if (j == -1) return i + 1
+      if (dict.contains((i, j))) return dict((i, j))
+
+      if (word1.charAt(i) == word2.charAt(j)) {
+        return dp(i - 1, j - 1)
+      } else {
+        val A = dp(i, j - 1) + 1 // 插入字符
+        val B = dp(i - 1, j) + 1 // 删除字符
+        val C = dp(i - 1, j - 1) + 1 // 替换字符
+        val result = Math.min(Math.min(A, B), C)
+        dict((i, j)) = result
+        result
+      }
+    }
+
+    dp(word1.length - 1, word2.length - 1)
   }
 }
