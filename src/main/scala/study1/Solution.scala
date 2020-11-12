3de6a82f19f8682ea6224f0096409aa9634d5f88
liu.chaobing <liu.chaobing@chinaredstar.com>
2020-11-12 13:35:48 +0800

    update

--- src/main/scala/study1/Solution.scala
+++ src/main/scala/study1/Solution.scala
@@ -23,7 +23,8 @@ object Solution {
     //    println(longestPalindromeSubseq("bbbab"))
     //    println(longestCommonSubsequence_bottom("abcde", "ace"))
     //    println("abc".substring(0, 0))
-    println(jump(Array(2, 3, 1, 1, 4)))
+    //    println(jump(Array(2, 3, 1, 1, 4)))
+    println(stoneGame(Array(5, 3, 4, 5)))
   }
 
   /*
@@ -629,5 +630,112 @@ s 只包含小写英文字母
       }
     }
     jump
+  }
+
+  /*
+  877. 石子游戏
+亚历克斯和李用几堆石子在做游戏。偶数堆石子排成一行，每堆都有正整数颗石子 piles[i] 。
+
+游戏以谁手中的石子最多来决出胜负。石子的总数是奇数，所以没有平局。
+
+亚历克斯和李轮流进行，亚历克斯先开始。 每回合，玩家从行的开始或结束处取走整堆石头。 这种情况一直持续到没有更多的石子堆为止，此时手中石子最多的玩家获胜。
+
+假设亚历克斯和李都发挥出最佳水平，当亚历克斯赢得比赛时返回 true ，当李赢得比赛时返回 false 。
+
+
+
+示例：
+
+输入：[5,3,4,5]
+输出：true
+解释：
+亚历克斯先开始，只能拿前 5 颗或后 5 颗石子 。
+假设他取了前 5 颗，这一行就变成了 [3,4,5] 。
+如果李拿走前 3 颗，那么剩下的是 [4,5]，亚历克斯拿走后 5 颗赢得 10 分。
+如果李拿走后 5 颗，那么剩下的是 [3,4]，亚历克斯拿走后 4 颗赢得 9 分。
+这表明，取前 5 颗石子对亚历克斯来说是一个胜利的举动，所以我们返回 true 。
+
+
+提示：
+
+2 <= piles.length <= 500
+piles.length 是偶数。
+1 <= piles[i] <= 500
+sum(piles) 是奇数。
+   */
+  def stoneGame_normal(piles: Array[Int]): Boolean = {
+    val n = piles.length
+    val dp = Array.ofDim[(Int, Int)](n, n)
+
+    for (i <- Range(n - 1, -1, -1)) {
+      for (j <- i until n) {
+        if (i == j) {
+          dp(i)(j) = (piles(i), 0)
+        } else {
+          val left = piles(i) + dp(i + 1)(j)._2
+          val right = piles(j) + dp(i)(j - 1)._2
+          if (left > right) {
+            dp(i)(j) = (left, dp(i + 1)(j)._1)
+          } else {
+            dp(i)(j) = (right, dp(i)(j - 1)._1)
+          }
+        }
+      }
+    }
+    dp(0)(n - 1)._1 > dp(0)(n - 1)._2
+  }
+
+  def stoneGame_normal2(piles: Array[Int]): Boolean = {
+    val n = piles.length
+    val dp = new Array[(Int, Int)](n)
+
+    for (i <- Range(n - 1, -1, -1)) {
+      for (j <- i until n) {
+        if (i == j) {
+          dp(j) = (piles(i), 0)
+        } else {
+          val left = piles(i) + dp(j)._2
+          val right = piles(j) + dp(j - 1)._2
+          if (left > right) {
+            dp(j) = (left, dp(j)._1)
+          } else {
+            dp(j) = (right, dp(j - 1)._1)
+          }
+        }
+      }
+    }
+    dp(n - 1)._1 > dp(n - 1)._2
+  }
+
+  def stoneGame_normal3(piles: Array[Int]): Boolean = {
+    val n = piles.length
+    val dp = Array.ofDim[Int](n, n)
+
+    for (i <- Range(n - 1, -1, -1)) {
+      for (j <- i until n) {
+        if (i == j) {
+          dp(i)(j) = piles(i)
+        } else {
+          dp(i)(j) = Math.max(piles(i) - dp(i + 1)(j), piles(j) - dp(i)(j - 1))
+        }
+      }
+    }
+    dp(0)(n - 1) > 0
+  }
+
+  def stoneGame(piles: Array[Int]): Boolean = {
+    val n = piles.length
+    val dp = new Array[Int](n)
+
+    for (i <- Range(n - 1, -1, -1)) {
+      for (j <- i until n) {
+        if (i == j) {
+          dp(j) = piles(i)
+        } else {
+          dp(j) = Math.max(piles(i) - dp(j), piles(j) - dp(j - 1))
+        }
+      }
+    }
+    dp(n - 1) > 0
   }
 }
