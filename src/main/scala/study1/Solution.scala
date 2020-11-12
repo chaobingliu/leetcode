dae928497f7fa3508dfb064c8d0537690a4b4dda
liu.chaobing <liu.chaobing@chinaredstar.com>
2020-11-12 10:48:03 +0800

    update

--- src/main/scala/study1/Solution.scala
+++ src/main/scala/study1/Solution.scala
@@ -22,7 +22,8 @@ object Solution {
     //    println(maxEnvelopes(Array(Array(5, 4), Array(6, 4), Array(6, 7), Array(2, 3))))
     //    println(longestPalindromeSubseq("bbbab"))
     //    println(longestCommonSubsequence_bottom("abcde", "ace"))
-    println("abc".substring(0, 0))
+    //    println("abc".substring(0, 0))
+    println(jump(Array(2, 3, 1, 1, 4)))
   }
 
   /*
@@ -551,5 +552,82 @@ s 只包含小写英文字母
       }
     }
     dp(m)(n)
+  }
+
+  /*
+55. 跳跃游戏
+给定一个非负整数数组，你最初位于数组的第一个位置。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+判断你是否能够到达最后一个位置。
+
+示例 1:
+
+输入: [2,3,1,1,4]
+输出: true
+解释: 我们可以先跳 1 步，从位置 0 到达 位置 1, 然后再从位置 1 跳 3 步到达最后一个位置。
+示例 2:
+
+输入: [3,2,1,0,4]
+输出: false
+解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
+   */
+  def canJump(nums: Array[Int]): Boolean = {
+    var farthest = 0
+    for (i <- 0 until nums.length - 1) {
+      farthest = Math.max(farthest, i + nums(i))
+      if (farthest <= i) return false
+    }
+    farthest >= nums.length - 1
+  }
+
+  /*
+  45. 跳跃游戏 II
+给定一个非负整数数组，你最初位于数组的第一个位置。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+
+示例:
+
+输入: [2,3,1,1,4]
+输出: 2
+解释: 跳到最后一个位置的最小跳跃数是 2。
+     从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+说明:
+
+假设你总是可以到达数组的最后一个位置。
+   */
+  def jump_normal(nums: Array[Int]): Int = {
+    val n = nums.length
+    val memo = Array.fill[Int](n)(n)
+
+    def dp(p: Int): Int = {
+      if (p >= n - 1)
+        return 0
+
+      if (memo(p) != n) return memo(p)
+      val step = nums(p)
+      for (i <- 1 to step) {
+        memo(p) = Math.min(memo(p), dp(p + i) + 1)
+      }
+      memo(p)
+    }
+
+    dp(0)
+  }
+
+  def jump(nums: Array[Int]): Int = {
+    var jump, end, farthest = 0
+    for (i <- 0 until nums.length - 1) {
+      farthest = Math.max(farthest, i + nums(i))
+      if (i == end) {
+        jump += 1
+        end = farthest
+      }
+    }
+    jump
   }
 }
