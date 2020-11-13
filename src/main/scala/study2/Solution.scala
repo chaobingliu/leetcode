43844dc598475d334698a46f933f92ce4df32b87
liu.chaobing <liu.chaobing@chinaredstar.com>
2020-11-13 14:46:41 +0800

    update

--- src/main/scala/study2/Solution.scala
+++ src/main/scala/study2/Solution.scala
@@ -127,4 +127,36 @@ k 是一个正整数，它的值小于或等于链表的长度。
     a.next = reverseKGroup(b, k)
     newHead
   }
+
+  /*
+  234. 回文链表
+请判断一个链表是否为回文链表。
+
+示例 1:
+
+输入: 1->2
+输出: false
+示例 2:
+
+输入: 1->2->2->1
+输出: true
+进阶：
+你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+   */
+  def isPalindrome(head: ListNode): Boolean = {
+    var slow, fast = head
+    while (fast != null && fast.next != null) {
+      slow = slow.next
+      fast = fast.next.next
+    }
+    if (fast != null) slow = slow.next
+    var left = head
+    var right = reverse(slow)
+    while (right != null) {
+      if (left.x != right.x) return false
+      left = left.next
+      right = right.next
+    }
+    true
+  }
 }
