80996930552af86e51111c5d760cf5a794799d2e
liu.chaobing <liu.chaobing@chinaredstar.com>
2020-11-16 20:56:07 +0800

    update

--- src/main/scala/study2/Solution.scala
+++ src/main/scala/study2/Solution.scala
@@ -833,5 +833,137 @@ nums1和nums2 的数组大小都不超过1000。
     }
   }
 
+  /*
+  875. 爱吃香蕉的珂珂
+珂珂喜欢吃香蕉。这里有 N 堆香蕉，第 i 堆中有 piles[i] 根香蕉。警卫已经离开了，将在 H 小时后回来。
+
+珂珂可以决定她吃香蕉的速度 K （单位：根/小时）。每个小时，她将会选择一堆香蕉，从中吃掉 K 根。如果这堆香蕉少于 K 根，她将吃掉这堆的所有香蕉，然后这一小时内不会再吃更多的香蕉。
+
+珂珂喜欢慢慢吃，但仍然想在警卫回来前吃掉所有的香蕉。
+
+返回她可以在 H 小时内吃掉所有香蕉的最小速度 K（K 为整数）。
+
+
+
+示例 1：
+
+输入: piles = [3,6,7,11], H = 8
+输出: 4
+示例 2：
+
+输入: piles = [30,11,23,4,20], H = 5
+输出: 30
+示例 3：
+
+输入: piles = [30,11,23,4,20], H = 6
+输出: 23
+
+
+提示：
+
+1 <= piles.length <= 10^4
+piles.length <= H <= 10^9
+1 <= piles[i] <= 10^9
+   */
+  def minEatingSpeed(piles: Array[Int], H: Int): Int = {
+
+    def canFinish(speed: Int): Boolean = {
+      var time = 0
+      for (n <- piles) {
+        time += ((if (n % speed == 0) 0 else 1) + n / speed)
+      }
+      time <= H
+    }
+
+    var left = 1
+    var right = piles.max + 1
+    while (left < right) {
+      val mid = left + (right - left) / 2
+      if (canFinish(mid)) {
+        right = mid
+      } else {
+        left = mid + 1
+      }
+    }
+    left
+  }
+
+  /*
+  1011. 在 D 天内送达包裹的能力
+传送带上的包裹必须在 D 天内从一个港口运送到另一个港口。
+
+传送带上的第 i 个包裹的重量为 weights[i]。每一天，我们都会按给出重量的顺序往传送带上装载包裹。我们装载的重量不会超过船的最大运载重量。
+
+返回能在 D 天内将传送带上的所有包裹送达的船的最低运载能力。
+
+
+
+示例 1：
+
+输入：weights = [1,2,3,4,5,6,7,8,9,10], D = 5
+输出：15
+解释：
+船舶最低载重 15 就能够在 5 天内送达所有包裹，如下所示：
+第 1 天：1, 2, 3, 4, 5
+第 2 天：6, 7
+第 3 天：8
+第 4 天：9
+第 5 天：10
+
+请注意，货物必须按照给定的顺序装运，因此使用载重能力为 14 的船舶并将包装分成 (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) 是不允许的。
+示例 2：
+
+输入：weights = [3,2,2,4,1,4], D = 3
+输出：6
+解释：
+船舶最低载重 6 就能够在 3 天内送达所有包裹，如下所示：
+第 1 天：3, 2
+第 2 天：2, 4
+第 3 天：1, 4
+示例 3：
+
+输入：weights = [1,2,3,1,1], D = 4
+输出：3
+解释：
+第 1 天：1
+第 2 天：2
+第 3 天：3
+第 4 天：1, 1
+
+
+提示：
+
+1 <= D <= weights.length <= 50000
+1 <= weights[i] <= 500
+   */
+  def shipWithinDays(weights: Array[Int], D: Int): Int = {
+    def canFinish(cap: Int): Boolean = {
+      var i = 0
+      for (day <- 1 to D) {
+        var maxCap = cap - weights(i)
+        while (maxCap >= 0) {
+          i += 1
+          if (i == weights.length) {
+            return true
+          }
+          maxCap -= weights(i)
+        }
+      }
+      false
+    }
+
+    var left = weights.max
+    var right = weights.sum + 1
+    while (left < right) {
+      val mid = left + (right - left) / 2
+      if (canFinish(mid)) {
+        right = mid
+      } else {
+        left = mid + 1
+      }
+    }
+    left
+  }
+
 }
 
