c007093c819e4fbd1940f0a945def44ad7738c66
liu.chaobing <liu.chaobing@chinaredstar.com>
2020-11-17 10:59:58 +0800

    update

--- src/main/scala/study2/Solution.scala
+++ src/main/scala/study2/Solution.scala
@@ -1174,6 +1174,37 @@ ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next 
     Array(-1, -1)
   }
 
+  /*
+  344. 反转字符串
+编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+
+不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+
+你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+
+
+
+示例 1：
+
+输入：["h","e","l","l","o"]
+输出：["o","l","l","e","h"]
+示例 2：
+
+输入：["H","a","n","n","a","h"]
+输出：["h","a","n","n","a","H"]
+   */
+  def reverseString(s: Array[Char]): Unit = {
+    var left = 0
+    var right = s.length - 1
+    while (left < right) {
+      val temp = s(left)
+      s(left) = s(right)
+      s(right) = temp
+      left += 1
+      right -= 1
+    }
+  }
+
 
 }
 
